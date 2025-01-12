ac6b680a9e36578ed09ae236612a1d5bde775692
Baker Sekitoleko <b.alzawad@gmail.com>
2025-01-12 16:26:32 +0300

    Feat: Tests the search component

--- src/__test__/Search.test.tsx
+++ src/__test__/Search.test.tsx
@@ -1,9 +1,26 @@
 import Search from "@/components/Search";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("Search", () => {
   test("renders the search field", () => {
-    const { container } = render(<Search searchParam="country" />);
-    expect(container).toMatchSnapshot();
+    render(<Search searchParam="country" />);
+
+    const elem = screen.queryByPlaceholderText("search by country ...");
+    expect(elem).toBeInTheDocument();
+  });
+
+  test("enters the input value in the search field", async () => {
+    render(<Search searchParam="country" />);
+    await waitFor(() =>
+      userEvent.type(
+        screen.getByPlaceholderText("search by country ..."),
+        "uganda"
+      )
+    );
+
+    expect(
+      screen.getByPlaceholderText("search by country ...")
+    ).toHaveDisplayValue("uganda");
   });
 });
